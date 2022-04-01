eb9645dd870d92b74411d66bb4811fa2eec5a263
Saeha <blancnoir.d@gmail.com>
2022-04-01 23:59:37 +0900

    Chore : MainActivity 추가

--- app/src/main/java/com/saeha/project/composebottomnavigationex/MainActivity.kt
+++ app/src/main/java/com/saeha/project/composebottomnavigationex/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBottomNavigationExTheme {
+            ComposeBottomNavigationExTheme { //이거는 Theme.kt에 있는거
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
